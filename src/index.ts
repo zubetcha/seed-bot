8810c4d508f14d7a1d1300a12c11d7ac7569320d
zubetcha <zuhye5@gmail.com>
2024-03-03 21:24:33 +0900

    Feat: 명단 정보 수정 기능

--- src/index.ts
+++ src/index.ts
@@ -15,6 +15,7 @@ type ContentsJoinReq = {
   nickname: string;
   content: string;
   team: number;
+  no?: number;
 };
 
 const app = express();
@@ -102,43 +103,53 @@ app.post('/contents/member/init', (req: Request<{ content: string }>, res) => {
 
 // 컨텐츠 가입
 app.post('/contents/member/join', (req: Request<ContentsJoinReq>, res) => {
-  const { nickname, content, team } = req.body;
+  const { nickname, content, team, no } = req.body;
   const index = team - 1;
   const fileName = MEMBER_LIST_FILE_NAME[content];
 
   const jsonStr = fs.readFileSync(getDataFilePath('list.json'), 'utf-8');
   const jsonObj = JSON.parse(jsonStr);
-  const contentList = jsonObj[content];
+  const contentList: ContentInfo[] = jsonObj[content];
 
   // 존재하는 보스인지 확인
   if (!contentList) {
-    return res.send(`${content}은/는 없습니다만!`);
+    return res.send(`${content}은/는 없습니다만.`);
   }
 
   // 존재하는 팀인지 확인
   if (!contentList[index]) {
-    return res.send(`${content} ${team}팀은 없습니다만!`);
+    return res.send(`${content} ${team}팀은 없습니다만.`);
   }
 
   const memberList: MemberList = contentList[index].members;
   // 꽉 찼는지 확인
   const isMax = memberList.filter(({ nickname }) => nickname).length === MAX_MEMBER_COUNT;
   if (isMax) {
-    return res.send(`${content} ${team}팀 꽉 참!`);
+    return res.send(`${content} ${team}팀 꽉 참.`);
   }
 
   // 중복 확인
   const duplicated = memberList.find((member) => member.nickname === nickname);
   if (duplicated) {
-    return res.send(`${nickname}은/는 이미 있음!`);
+    return res.send(`${nickname}은/는 이미 있음.`);
   }
 
-  let no = 1;
+  // no 확인
+  if (no && (no > 10 || no < 1)) {
+    return res.send('번호는 1부터 10까지만 작성.');
+  }
+
+  // no 자리 비어 있는지 확인
+  if (contentList[index].members[no - 1].nickname) {
+    return res.send(`${no}번 자리는 이미 있습니다만.`);
+  }
+
+  let newNo = no || 1;
   const newList = contentList.map((info: ContentInfo, i: number) => {
-    if (i === team - 1) {
-      const firstEmptyIdx = info.members.findIndex(({ nickname }) => !nickname);
-      const newMembers = info.members.map((member, i) => (i === firstEmptyIdx ? { ...member, nickname } : member));
-      no = firstEmptyIdx + 1;
+    if (i === index) {
+      const targetIdx = no ? no - 1 : info.members.findIndex(({ nickname }) => !nickname);
+      const newMembers = info.members.map((member, i) => (i === targetIdx ? { ...member, nickname } : member));
+      newNo = targetIdx + 1;
 
       return { ...info, members: newMembers };
     }
@@ -149,7 +160,7 @@ app.post('/contents/member/join', (req: Request<ContentsJoinReq>, res) => {
   const newJson = { ...jsonObj, [content]: newList };
   const htmlStr = fs.readFileSync(getDataFilePath(fileName), 'utf-8');
   const $ = cheerio.load(htmlStr, null, false);
-  const targetId = `#${content}-${team}-${no}`;
+  const targetId = `#${content}-${team}-${newNo}`;
 
   $(targetId).append(` ${nickname}`);
 
@@ -161,23 +172,76 @@ app.post('/contents/member/join', (req: Request<ContentsJoinReq>, res) => {
   res.send(listHtml);
 });
 
-// TODO: 컨텐츠 명단 수정
-app.post('/contents/member/edit', (req, res) => {
-  res.send('get gundan');
-});
-
 // 컨텐츠 시간 및 날짜 수정
-app.post(
-  '/contents/member/edit',
-  (req: Request<{ content: string; team: number; key: string; value: string }>, res) => {
-    const { content, team, key, value } = req.body;
-    if (key === '시간') {
-    } else if (!isNaN(Number(key))) {
-    }
+app.post('/contents/edit', (req: Request<{ content: string; team: number; key: string; value: string }>, res) => {
+  const { content, team, key, value } = req.body;
+  const index = team - 1;
 
-    res.send('get gundan');
+  const jsonStr = fs.readFileSync(getDataFilePath('list.json'), 'utf-8');
+  const jsonObj = JSON.parse(jsonStr);
+  const contentList: ContentInfo[] = jsonObj[content];
+  const fileName = MEMBER_LIST_FILE_NAME[content];
+
+  if (!value) {
+    return res.send('수정하려는 값을 같이 작성해 주셔야죠?');
   }
-);
+
+  // 존재하는 보스인지 확인
+  if (!contentList) {
+    return res.send(`${content}은/는 없습니다만!`);
+  }
+
+  // 존재하는 팀인지 확인
+  if (!contentList[index]) {
+    return res.send(`${content} ${team}팀은 없습니다만!`);
+  }
+
+  const htmlStr = fs.readFileSync(getDataFilePath(fileName), 'utf-8');
+  const $ = cheerio.load(htmlStr, null, false);
+  let newContentList = contentList;
+
+  if (key === '시간') {
+    const targetId = `#${content}-${team}-time`;
+
+    newContentList = contentList.map((info, i) => {
+      if (i === index) {
+        return { ...info, startTime: value };
+      }
+
+      return info;
+    });
+
+    const newText = $(targetId).text().replace(`${contentList[index].startTime}`, value);
+    $(targetId).empty().append(newText);
+  } else if (!isNaN(Number(key))) {
+    const no = Number(key);
+    const targetId = `#${content}-${team}-${no}`;
+
+    newContentList = contentList.map((info, i) => {
+      const index = Number(key) - 1;
+      const newMemberList = info.members.map((member, j) => (j === no - 1 ? { nickname: value } : member));
+
+      if (i === index) {
+        return { ...info, members: newMemberList };
+      }
+
+      return info;
+    });
+
+    const newText = $(targetId)
+      .text()
+      .replace(`${contentList[index].members[no - 1].nickname}`, value);
+    $(targetId).empty().append(newText);
+  }
+
+  const newJson = { ...jsonObj, [content]: newContentList };
+  const listHtml = refineListHtml($, content);
+
+  fs.writeFileSync(getDataFilePath('list.json'), JSON.stringify(newJson, null, 2));
+  fs.writeFileSync(getDataFilePath(fileName), $.html());
+
+  res.send(listHtml);
+});
 
 // 컨텐츠 명단 삭제
 app.post('/contents/member/delete', (req: Request<{ content: string; team: number; no: number }>, res) => {
@@ -232,8 +296,6 @@ app.post('/contents/member/delete', (req: Request<{ content: string; team: numbe
 
   res.send(listHtml);
 });
-
-// 명단 초기화
 
 app.listen(port, () => {
   console.log(`seed-bot listening on port ${port}`);
--- src/script/messenger.js
+++ src/script/messenger.js
@@ -23,16 +23,93 @@ const getContentsMemberList = (content) => {
   return result;
 };
 
-const joinContents = ({ nickname, content, order }) => {
+const initContent = (content) => {
   let json;
   let result;
   try {
-    let response = org.jsoup.Jsoup.connect(BASE_URL + '/contents/member')
+    let response = org.jsoup.Jsoup.connect(BASE_URL + '/contents/member/init')
       .header('Content-Type', 'application/json')
       .data('content', content)
       .ignoreContentType(true)
       .ignoreHttpErrors(true)
-      .get();
+      .post();
+
+    result = org.jsoup.Jsoup.parse(response.html())
+      .body()
+      .html()
+      .replace(/<br\s*\/?>/gi, '');
+  } catch (e) {
+    result = e;
+    Log.e(e);
+  }
+
+  return result;
+};
+
+const joinContent = ({ nickname, content, team, no }) => {
+  let json;
+  let result;
+  try {
+    let response = org.jsoup.Jsoup.connect(BASE_URL + '/contents/member/join')
+      .header('Content-Type', 'application/json')
+      .data('nickname', nickname)
+      .data('content', content)
+      .data('team', team)
+      .data('no', no ?? null)
+      .ignoreContentType(true)
+      .ignoreHttpErrors(true)
+      .post();
+
+    result = org.jsoup.Jsoup.parse(response.html())
+      .body()
+      .html()
+      .replace(/<br\s*\/?>/gi, '');
+  } catch (e) {
+    result = e;
+    Log.e(e);
+  }
+
+  return result;
+};
+
+const editContentInfo = ({ content, team, key, value }) => {
+  let json;
+  let result;
+  try {
+    let response = org.jsoup.Jsoup.connect(BASE_URL + '/contents/edit')
+      .header('Content-Type', 'application/json')
+      .data('content', content)
+      .data('team', team)
+      .data('key', key)
+      .data('value', value)
+      .ignoreContentType(true)
+      .ignoreHttpErrors(true)
+      .post();
+
+    result = org.jsoup.Jsoup.parse(response.html())
+      .body()
+      .html()
+      .replace(/<br\s*\/?>/gi, '');
+  } catch (e) {
+    result = e;
+    Log.e(e);
+  }
+
+  return result;
+};
+
+const deleteMember = () => {
+  let json;
+  let result;
+  try {
+    let response = org.jsoup.Jsoup.connect(BASE_URL + '/contents/member/delete')
+      .header('Content-Type', 'application/json')
+      .data('content', content)
+      .data('team', team)
+      .data('no', no)
+      .ignoreContentType(true)
+      .ignoreHttpErrors(true)
+      .post();
 
     result = org.jsoup.Jsoup.parse(response.html())
       .body()
@@ -51,7 +128,7 @@ function response(room, msg, sender, isGroupChat, replier, imageDB, packazgeName
     return;
   }
 
-  const [cmd, content, time] = msg.trim().split(' ');
+  const [cmd, content, team, key, value] = msg.trim().split(' ');
 
   if (cmd === '!명단') {
     const list = getContentsMemberList(content);
@@ -60,15 +137,48 @@ function response(room, msg, sender, isGroupChat, replier, imageDB, packazgeName
     replier.reply(room, list);
   }
 
-  if (cmd === '!가입') {
-    const contentStr = {
-      군단: '군단',
-      카룻: '카룻',
-    };
+  if (cmd === '!초기화') {
+    const list = initContent(content);
 
-    replier.reply(contentStr[content]);
+    Log.i(list);
+    replier.reply(room, list);
+  }
+
+  if (cmd === '!가입') {
+    const data = {
+      nickname: key ?? sender,
+      content,
+      team: Number(team),
+      no: Number(value),
+    };
+    const list = joinContent(data);
+
+    Log.i(list);
+    replier.reply(room, list);
   }
 
   if (cmd === '!수정') {
+    const data = {
+      content,
+      team: Number(team),
+      key,
+      value,
+    };
+    const list = editContentInfo(data);
+
+    Log.i(list);
+    replier.reply(room, list);
+  }
+
+  if (cmd === '!탈퇴') {
+    const data = {
+      content,
+      team: Number(team),
+      no: Number(key),
+    };
+    const list = deleteMember(data);
+
+    Log.i(list);
+    replier.reply(room, list);
   }
 }
